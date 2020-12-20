b46d6c023867245a101635cef5f483bdaaa7ff6a
wernertimothy <wernertimo@live.com>
2020-12-20 12:15:23 +0100

    add python/2020/Day18

---
+++ 2020/python/Day18_Part1.py
@@ -0,0 +1,31 @@
+
+def main():
+    homework = ReadPuzzleInput('PuzzleInput_Day18.txt')
+    accumulator = 0
+    for expr in homework:
+        accumulator += evaluate(expr.replace('*','-'))
+    print('The solution is', accumulator)
+
+class number:
+    def __init__(self, x):
+        self.x = x
+    def __add__(self, y):
+        return number(self.x + y.x)
+    def __sub__(self, y):
+        return number(self.x * y.x)
+
+def evaluate(expression):
+    exp = ''
+    for char in expression:
+        if char in '0123456789':
+            exp += 'number(' + char + ')'
+        else:
+            exp += char
+    return(eval(exp).x) 
+
+def ReadPuzzleInput(my_file):
+    with open(my_file, 'r') as f:
+        return [line.rstrip() for line in f]
+
+if __name__ == "__main__":
+    main()
